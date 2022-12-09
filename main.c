19156a073c44c0db95dec06db76a655f435f3f0b
CodeFreak2002 <ashwinarun2002@gmail.com>
2022-12-09 21:44:03 +0530

    Added parser for declarations

--- main.c
+++ main.c
@@ -10,7 +10,15 @@
 #define EXIT_SUCCESS 0
 
 char tokens[100000][50];
-int cur_pos = 0, token_pos = 0;
+int cur_pos = 0, token_pos = 0, end_pos=0;
+
+struct Node{
+
+    char val[50];
+    int child_cnt;
+    struct Node* child[15];
+
+};
 
 /**
 Reads the passed input file line by line.
@@ -86,6 +94,153 @@ int tokenize(char *filename) {
     return cur_pos;  
 }
 
+void addChild(struct Node* par, struct Node* ch)
+{
+    if (ch==NULL)
+        return;
+
+    par->child[par->child_cnt++] = ch; 
+}
+
+void error()
+{
+    perror("Error occurred\n");
+    exit(EXIT_FAILURE);
+}
+
+struct Node* createNode()
+{
+    return (struct Node*)(malloc(sizeof(struct Node)));
+}
+
+// all identifier functions are defined here
+
+bool isDeclaration()
+{
+    if (strcmp("int", tokens[cur_pos])==0)
+        return true;
+    
+    return false;
+}
+
+bool isVariable()
+{
+    char* v = tokens[cur_pos];
+
+    if (strcmp(v,"for")==0)
+        return false;
+
+    if (strcmp(v,"int")==0)
+        return false;
+
+    if (strcmp(v,"read")==0)
+        return false;
+
+    if (strcmp(v,"write")==0)
+        return false;
+
+    return isalpha(v);
+}
+
+// all parser functions are defined here
+
+struct Node* parseTerminal()
+{
+    struct Node* t = (struct Node*)(malloc(sizeof(struct Node)));
+    strcpy(t->val,tokens[cur_pos++]);
+    return t;
+}
+
+struct Node* parseVariable(int spos)
+{
+    char* s = tokens[cur_pos];
+    int l = strlen(s);
+
+    if (spos==l)
+        return NULL;
+
+    struct Node* I = (struct Node*)(malloc(sizeof(struct Node)));
+    struct Node* C = (struct Node*)(malloc(sizeof(struct Node)));
+    strcpy(C->val, "C");
+
+    struct Node* t = createNode();
+    t->val[0] = s[spos];
+
+    addChild(C, t);
+    addChild(I, C);
+    addChild(I, parseVariable(spos+1));
+    return I;
+}
+
+struct Node* parseVariableList()
+{
+    struct Node* L = (struct Node*)(malloc(sizeof(struct Node)));
+
+    if (isVariable())
+        addChild(L, parseVariable(0));
+    else {
+        error();
+        return NULL;
+    }
+
+    if (strcmp(tokens[cur_pos],",")==0)
+    {
+        addChild(L, parseTerminal());
+        struct Node* x = parseVariableList();
+        addChild(L, x);
+        return L;
+    }
+    else if (strcmp(tokens[cur_pos],";")==0)
+        return NULL;
+    else
+    {
+        error();
+        return NULL;
+    }
+}
+
+struct Node* parseDeclaration()
+{
+    struct Node* D= (struct Node*)(malloc(sizeof(struct Node)));
+    addChild(D, parseTerminal());
+    addChild(D, parseVariableList());
+    return D;
+}
+
+struct Node* parseProgram()
+{
+    // end of program
+
+    if (cur_pos == end_pos)
+        return NULL;
+
+    // ignore empty statements
+
+    if (strcmp(tokens[cur_pos], ";")==0)
+    {
+        cur_pos++;
+        parseProgram();
+    }
+
+    struct Node* P = (struct Node*)(malloc(sizeof(struct Node)));
+    strcpy(P->val, "P");
+
+    if (isDeclaration())
+        addChild(P, parseDeclaration());
+
+    if (strcmp(tokens[cur_pos],";")==0)
+        addChild(P, parseTerminal());
+    else
+    {
+        error();
+        return NULL;
+    }
+
+    addChild(P, parseProgram());
+    return P;
+}
+
+
 int main(int argc, char** argv) {
 
     if (argc < 2) {
@@ -94,6 +249,8 @@ int main(int argc, char** argv) {
     }
 
     printf("%d" , tokenize(argv[1]));
+    end_pos = tokenize(argv[1]);
+    cur_pos = 0;
 
     return EXIT_SUCCESS;
 }
