cb9ad1441a34f170374ade5551bc7482c5ffdb34
Chinmay Dalal <dalal.chinmay.0101@gmail.com>
2022-12-10 18:05:20 +0530

    format

--- main.c
+++ main.c
@@ -1,29 +1,28 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <math.h>
 #include <ctype.h>
+#include <math.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define MAX_TOKENS (1000000)
 #define MAX_LINE_LENGTH (1000000)
 #define EXIT_SUCCESS 0
 
 char tokens[100000][50];
-int cur_pos = 0, token_pos = 0, end_pos=0;
+int cur_pos = 0, token_pos = 0, end_pos = 0;
 
-struct Node{
+struct Node {
 
     char val[50];
     int child_cnt;
-    struct Node* child[15];
-
+    struct Node *child[15];
 };
 
 /**
 Reads the passed input file line by line.
-Removes all space type characters from the lines, including \n (Carriage Return). 
-Modifies the global tokens and num_tokens variables. 
+Removes all space type characters from the lines, including \n (Carriage
+Return). Modifies the global tokens and num_tokens variables.
 */
 
 // We need to split on delimiters , while pushing certain delimiters as tokens
@@ -31,24 +30,26 @@ Modifies the global tokens and num_tokens variables.
 // \n whitespace \t \0 -> not be pushed
 
 int charCheck(char c) {
-    if(c == ',' || c == ';' || c == '(' || c == ')' || c == '{' || c == '}')
+    if (c == ',' || c == ';' || c == '(' || c == ')' || c == '{' || c == '}')
         return 1;
-    else if (c == '\n' || c == '\t' || c == '\0' || c == ' ' || c == 0 || c == 10 || c == 13) 
+    else if (c == '\n' || c == '\t' || c == '\0' || c == ' ' || c == 0 ||
+             c == 10 || c == 13)
         return 2;
     return 0;
 }
 
-char operators[6] = {'+' , '-' , '*' , '/' , '>' , '='};
+char operators[6] = {'+', '-', '*', '/', '>', '='};
 
 int tokenize(char *filename) {
-    FILE* in_file = fopen(filename , "r");
+    FILE *in_file = fopen(filename, "r");
     char line[MAX_LINE_LENGTH];
     while (fgets(line, MAX_LINE_LENGTH, in_file)) {
-        for (int i = 0 ; i < strlen(line) ; i++) {
+        for (int i = 0; i < strlen(line); i++) {
             bool isoperator = false;
-            for (int j = 0 ; j < 6 ; j++) {
+            for (int j = 0; j < 6; j++) {
                 if (operators[j] == line[i]) {
-                    if (operators[j] == '=' && cur_pos > 0 && tokens[cur_pos-1][0] == '=' && line[i-1] == '=') {
+                    if (operators[j] == '=' && cur_pos > 0 &&
+                        tokens[cur_pos - 1][0] == '=' && line[i - 1] == '=') {
                         tokens[cur_pos - 1][1] = '=';
                         isoperator = true;
                         break;
@@ -57,8 +58,7 @@ int tokenize(char *filename) {
                         tokens[++cur_pos][0] = operators[j];
                         token_pos = 0;
                         cur_pos++;
-                    } 
-                    else {
+                    } else {
                         tokens[cur_pos][0] = operators[j];
                         token_pos = 0;
                         cur_pos++;
@@ -70,49 +70,45 @@ int tokenize(char *filename) {
 
             if (isoperator)
                 continue;
-            
+
             int type = charCheck(line[i]);
             if (type == 2) {
                 if (token_pos > 0)
                     cur_pos++;
                 token_pos = 0;
             } else if (type == 1) {
-                if (token_pos > 0) 
+                if (token_pos > 0)
                     cur_pos++;
                 tokens[cur_pos][0] = line[i];
                 cur_pos++;
                 token_pos = 0;
-            } else 
+            } else
                 tokens[cur_pos][token_pos++] = line[i];
         }
-    }  
+    }
 
-    return cur_pos;  
+    return cur_pos;
 }
 
-void addChild(struct Node* par, struct Node* ch)
-{
-    if (ch==NULL)
+void addChild(struct Node *par, struct Node *ch) {
+    if (ch == NULL)
         return;
 
     int c = par->child_cnt++;
-    struct Node** p = par->child;
+    struct Node **p = par->child;
     p[c] = ch;
 }
 
-void error()
-{
+void error() {
     perror("Error occurred\n");
     exit(EXIT_FAILURE);
 }
 
-struct Node* createNode()
-{
-    return (struct Node*)(malloc(sizeof(struct Node)));
+struct Node *createNode() {
+    return (struct Node *)(malloc(sizeof(struct Node)));
 }
 
-void printTree(struct Node* root)
-{
+void printTree(struct Node *root) {
     printf("%s ", root->val);
     int c = root->child_cnt;
 
@@ -120,42 +116,39 @@ void printTree(struct Node* root)
         return;
 
     printf("[");
-    for (int i=0;i<c;i++)
+    for (int i = 0; i < c; i++)
         printTree(root->child[i]);
     printf("]");
 }
 
 // all identifier functions are defined here
 
-bool isDeclaration()
-{
-    if (strcmp("int", tokens[cur_pos])==0)
+bool isDeclaration() {
+    if (strcmp("int", tokens[cur_pos]) == 0)
         return true;
-    
+
     return false;
 }
 
-bool isVariable()
-{
-    char* v = tokens[cur_pos];
+bool isVariable() {
+    char *v = tokens[cur_pos];
 
-    if (strcmp(v,"for")==0)
+    if (strcmp(v, "for") == 0)
         return false;
 
-    if (strcmp(v,"int")==0)
+    if (strcmp(v, "int") == 0)
         return false;
 
-    if (strcmp(v,"read")==0)
+    if (strcmp(v, "read") == 0)
         return false;
 
-    if (strcmp(v,"write")==0)
+    if (strcmp(v, "write") == 0)
         return false;
 
     int l = strlen(v);
-    
-    for (int i=0;i<l;i++)
-    {
-        if (v[i]<'a' || v[i]>'z')   
+
+    for (int i = 0; i < l; i++) {
+        if (v[i] < 'a' || v[i] > 'z')
             return false;
     }
 
@@ -164,70 +157,62 @@ bool isVariable()
 
 // all parser functions are defined here
 
-struct Node* parseTerminal()
-{
-    struct Node* t = (struct Node*)(malloc(sizeof(struct Node)));
-    strcpy(t->val,tokens[cur_pos++]);
+struct Node *parseTerminal() {
+    struct Node *t = (struct Node *)(malloc(sizeof(struct Node)));
+    strcpy(t->val, tokens[cur_pos++]);
     return t;
 }
 
-struct Node* parseVariable(int spos)
-{
-    char* s = tokens[cur_pos];
+struct Node *parseVariable(int spos) {
+    char *s = tokens[cur_pos];
     int l = strlen(s);
 
-    if (spos==l)
+    if (spos == l) {
         return NULL;
+    }
 
-    struct Node* I = (struct Node*)(malloc(sizeof(struct Node)));
-    strcpy(I->val,"I");
+    struct Node *I = (struct Node *)(malloc(sizeof(struct Node)));
+    strcpy(I->val, "I");
 
-    struct Node* C = (struct Node*)(malloc(sizeof(struct Node)));
+    struct Node *C = (struct Node *)(malloc(sizeof(struct Node)));
     strcpy(C->val, "C");
 
-    struct Node* t = createNode();
+    struct Node *t = createNode();
     t->val[0] = s[spos];
 
     addChild(C, t);
     addChild(I, C);
-    addChild(I, parseVariable(spos+1));
+    addChild(I, parseVariable(spos + 1));
     return I;
 }
 
-struct Node* parseVariableList()
-{
-    struct Node* L = (struct Node*)(malloc(sizeof(struct Node)));
-    strcpy(L->val,"L");
+struct Node *parseVariableList() {
+    struct Node *L = (struct Node *)(malloc(sizeof(struct Node)));
+    strcpy(L->val, "L");
 
-    if (isVariable())
-    {
+    if (isVariable()) {
         addChild(L, parseVariable(0));
         cur_pos++;
+    } else {
+        error();
+        return NULL;
     }
+
+    if (strcmp(tokens[cur_pos], ",") == 0) {
+        addChild(L, parseTerminal());
+        struct Node *x = parseVariableList();
+        addChild(L, x);
+        return L;
+    } else if (strcmp(tokens[cur_pos], ";") == 0)
+        return L;
     else {
         error();
         return NULL;
     }
-
-    if (strcmp(tokens[cur_pos],",")==0)
-    {
-        addChild(L, parseTerminal());
-        struct Node* x = parseVariableList();
-        addChild(L, x);
-        return L;
-    }
-    else if (strcmp(tokens[cur_pos],";")==0)
-        return L;
-    else
-    {
-        error();
-        return NULL;
-    }
 }
 
-struct Node* parseDeclaration()
-{
-    struct Node* D= (struct Node*)(malloc(sizeof(struct Node)));
+struct Node *parseDeclaration() {
+    struct Node *D = (struct Node *)(malloc(sizeof(struct Node)));
     strcpy(D->val, "D");
 
     addChild(D, parseTerminal());
@@ -235,8 +220,7 @@ struct Node* parseDeclaration()
     return D;
 }
 
-struct Node* parseProgram()
-{
+struct Node *parseProgram() {
     // end of program
 
     if (cur_pos == end_pos)
@@ -244,24 +228,21 @@ struct Node* parseProgram()
 
     // ignore empty statements
 
-    if (strcmp(tokens[cur_pos], ";")==0)
-    {
+    if (strcmp(tokens[cur_pos], ";") == 0) {
         cur_pos++;
         return parseProgram();
     }
 
-    struct Node* P = (struct Node*)(malloc(sizeof(struct Node)));
+    struct Node *P = (struct Node *)(malloc(sizeof(struct Node)));
     strcpy(P->val, "P");
 
-    if (isDeclaration())
-    {
+    if (isDeclaration()) {
         addChild(P, parseDeclaration());
     }
 
-    if (strcmp(tokens[cur_pos],";")==0)
+    if (strcmp(tokens[cur_pos], ";") == 0)
         addChild(P, parseTerminal());
-    else
-    {
+    else {
         error();
         return NULL;
     }
@@ -270,22 +251,22 @@ struct Node* parseProgram()
     return P;
 }
 
-
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
 
     if (argc < 2) {
-        perror("Pass the name of the input file as the first parameter. e.g.: ./simulator input.txt");
+        perror("Pass the name of the input file as the first parameter. e.g.: "
+               "./simulator input.txt");
         exit(EXIT_FAILURE);
     }
 
     end_pos = tokenize(argv[1]);
 
-    for (int i=0;i<end_pos;i++)
-        printf("%s\n",tokens[i]);
+    for (int i = 0; i < end_pos; i++)
+        printf("%s\n", tokens[i]);
 
     cur_pos = 0;
 
-    struct Node* root = parseProgram();
+    struct Node *root = parseProgram();
     printTree(root);
     printf("\n");
 
